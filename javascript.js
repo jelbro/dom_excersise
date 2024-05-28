374f5f2e7ff719c4f85ea8f82c86ddec94bd8507
Jelbro <Jelbro97@gmail.com>
2024-05-28 15:44:16 +0100

    Add blue h3

--- javascript.js
+++ javascript.js
@@ -11,3 +11,9 @@ paragraph.style.color = 'red';
 paragraph.textContent = "Hey I'm red!";
 
 container.appendChild(paragraph);
+
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = "I'm a blue h3!";
+
+container.appendChild(h3);
