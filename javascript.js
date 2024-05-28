a3fa236f8e2d3a1365f29d723c1d519543332f88
Jelbro <Jelbro97@gmail.com>
2024-05-28 16:15:56 +0100

    Add p and h1 to div

--- javascript.js
+++ javascript.js
@@ -17,3 +17,23 @@ h3.style.color = 'blue';
 h3.textContent = "I'm a blue h3!";
 
 container.appendChild(h3);
+
+const div = document.createElement('div');
+
+div.style['borderStyle'] = 'solid';
+div.style.borderColor = 'black';
+div.style['backgroundColor'] = 'pink';
+
+const div_h1 = document.createElement('h1');
+
+div_h1.textContent = "I'm in a div";
+
+div.appendChild(div_h1);
+
+const div_p = document.createElement('p');
+
+div_p.textContent = 'ME TOO!';
+
+div.appendChild(div_p);
+
+container.appendChild(div);
